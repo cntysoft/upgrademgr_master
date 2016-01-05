f6b9b415d1d8243589e2c319aea3370a152a6283
SOFTBOY <softboy@fhzc.com>
2016-01-05 18:25:59 +0800

    修复遗留bug

--- src/apps/upgrademgr_master/api/repo/info.cpp
+++ src/apps/upgrademgr_master/api/repo/info.cpp
@@ -26,16 +26,16 @@ Info::Info(ApiProvider &provider)
 
 ApiInvokeResponse Info::lsSoftwareRepoDir(const ApiInvokeRequest &request)
 {
+   int baseLength = m_dataDir.size()+1;
    QStringList list;
-   Filesystem::traverseFs(m_dataDir, 1, [&list](QFileInfo &fileInfo, int){
-      list << fileInfo.baseName();
+   Filesystem::traverseFs(m_dataDir, 1, [&list, baseLength](QFileInfo &fileInfo, int){
+      list << fileInfo.absoluteFilePath().remove(0, baseLength);
    });
    QBuffer buffer;
    buffer.open(QIODevice::WriteOnly);
    QDataStream out(&buffer);
    out << list;
-   ApiInvokeResponse response("Repo/Info/lsSoftwareRepoDir", false);
-   response.setError({1, "asdasdasdas"});
+   ApiInvokeResponse response("Repo/Info/lsSoftwareRepoDir", true);
    response.setExtraData(buffer.data());
    response.setSerial(request.getSerial());
    return response;
